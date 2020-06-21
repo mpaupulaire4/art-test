4d3f068f31de09bab6d807ba931a3ed85dae1fbf
Michel Paupulaire <mpaupulaire4@gmail.com>
2020-06-21 11:24:04 -0600

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import KnowledgeCheck from './KnowlegeCheck'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App({ blocks = [{type: 'KnowledgeCheck' }] } = {}) {
+  return blocks.map((block) => {
+    const Component = BLOCKS[block.type]
+    return (
+      <Component {...block.data}/>
+    );
+  })
+}
+
+const BLOCKS = {
+  KnowledgeCheck,
 }
 
 export default App;
